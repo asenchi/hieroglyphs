eeab8afc0114191d207752cfed336c15f2deb25a
Curt Micol <asenchi+github@asenchi.com>
2014-01-22 11:19:22 -0500

    Parse the timestamp into a time.Time struct

--- hieroglyphs_test.go
+++ hieroglyphs_test.go
@@ -2,14 +2,16 @@ package hieroglyphs
 
 import (
 	"testing"
+	"time"
 )
 
+var loc, _ = time.LoadLocation("America/Los_Angeles")
 var testEvents = []struct {
 	RawMessage string
 	Event      *LogEvent
 }{
-	{"<86>1 2014-01-20T13:26:16-08:00 rappel-fe1-pe1-prd sudo - - - pam_unix(sudo:session): session closed for user root", &LogEvent{Priority: 86, Version: 1, Severity: 6, Facility: 80, Timestamp: "2014-01-20T13:26:16-08:00", Hostname: "rappel-fe1-pe1-prd", Program: "sudo", Pid: "-", Message: "pam_unix(sudo:session): session closed for user root", Original: []byte("<86>1 2014-01-20T13:26:16-08:00 rappel-fe1-pe1-prd sudo - - - pam_unix(sudo:session): session closed for user root")}},
-	{"<86>1 2014-01-20T13:26:16-08:00 rappel-fe1-pe1-prd sshd 16385 - - Received disconnect from 127.0.0.1: 11: disconnected by user", &LogEvent{Priority: 86, Version: 1, Severity: 6, Facility: 80, Timestamp: "2014-01-20T13:26:16-08:00", Hostname: "rappel-fe1-pe1-prd", Program: "sshd", Pid: "16385", Message: "Received disconnect from 127.0.0.1: 11: disconnected by user", Original: []byte("<86>1 2014-01-20T13:26:16-08:00 rappel-fe1-pe1-prd sshd 16385 - - Received disconnect from 127.0.0.1: 11: disconnected by user")}},
+	{"<86>1 2014-01-20T13:26:16-08:00 rappel-fe1-pe1-prd sudo - - - pam_unix(sudo:session): session closed for user root", &LogEvent{Priority: 86, Version: 1, Severity: 6, Facility: 80, Timestamp: time.Date(2014, 01, 20, 13, 26, 16, 0, loc), Hostname: "rappel-fe1-pe1-prd", Program: "sudo", Pid: "-", Message: "pam_unix(sudo:session): session closed for user root", Original: []byte("<86>1 2014-01-20T13:26:16-08:00 rappel-fe1-pe1-prd sudo - - - pam_unix(sudo:session): session closed for user root")}},
+	{"<86>1 2014-01-20T13:26:16-08:00 rappel-fe1-pe1-prd sshd 16385 - - Received disconnect from 127.0.0.1: 11: disconnected by user", &LogEvent{Priority: 86, Version: 1, Severity: 6, Facility: 80, Timestamp: time.Date(2014, 01, 20, 13, 26, 16, 0, loc), Hostname: "rappel-fe1-pe1-prd", Program: "sshd", Pid: "16385", Message: "Received disconnect from 127.0.0.1: 11: disconnected by user", Original: []byte("<86>1 2014-01-20T13:26:16-08:00 rappel-fe1-pe1-prd sshd 16385 - - Received disconnect from 127.0.0.1: 11: disconnected by user")}},
 }
 
 func TestEventParsing(t *testing.T) {
